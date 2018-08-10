bc8393bc16879e12a805302b340152298ee6e0dc
Shubham Singh <shubham.singh@mountblue.io>
2018-08-10 21:29:18 +0530

    google auth using localhost done

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	},
+
+	'googleAuth' : {
+		'clientID': '1042483086839-13p4le1nemr8kfj1bhb91tftervh3oo3.apps.googleusercontent.com',
+		'clientSecret': '5xPYDLwylzqa_Pm83EuttEuf',
+		'callbackURL': 'http://ec2-18-222-248-47.us-east-2.compute.amazonaws.com:8080/auth/google/callback'
+	}
+}
